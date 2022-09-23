fe5a38e14ec4746c3217b05a0ce63ca72a5ba7c3
Muhammad Hamadto <muhamadto@gmail.com>
2022-09-23 21:43:14 +1000

    Fix test for `CodeQL` action

--- src/test/java/com/coffeebeans/springnativeawslambda/SpringNativeAwsLambdaApplicationTest.java
+++ src/test/java/com/coffeebeans/springnativeawslambda/SpringNativeAwsLambdaApplicationTest.java
@@ -1,5 +1,7 @@
 package com.coffeebeans.springnativeawslambda;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
