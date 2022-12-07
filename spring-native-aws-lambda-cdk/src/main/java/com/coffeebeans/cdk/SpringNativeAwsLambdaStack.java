8d105b648b91cfb6397adf254c16d822f1c2d977
Matto <muhamadto@gmail.com>
2022-12-08 00:11:34 +1100

    Fixed cdk Test
    Signed-off-by: Matto <muhamadto@gmail.com>

--- spring-native-aws-lambda-cdk/src/main/java/com/coffeebeans/cdk/SpringNativeAwsLambdaStack.java
+++ spring-native-aws-lambda-cdk/src/main/java/com/coffeebeans/cdk/SpringNativeAwsLambdaStack.java
@@ -30,7 +30,7 @@ public class SpringNativeAwsLambdaStack extends ApiBaseStack {
   private static final String SQS_SUCCESS_QUEUE_ID = "spring-native-aws-lambda-function-success-queue";
   private static final String SQS_FAILURE_QUEUE_ID = "spring-native-aws-lambda-function-failure-queue";
   private static final String LAMBDA_HANDLER = "org.springframework.cloud.function.adapter.aws.FunctionInvoker::handleRequest";
-  private static final AssetCode ASSET_CODE = fromAsset("../" + LAMBDA_FUNCTION_ID + "/target/spring-native-aws-lambda-function-native-zip.zip");
+  private static final AssetCode ASSET_CODE = fromAsset(LAMBDA_FUNCTION_ID + "/target/spring-native-aws-lambda-function-native-zip.zip");
   private static final String ENVIRONMENT_VARIABLE_SPRING_PROFILES_ACTIVE = "SPRING_PROFILES_ACTIVE";
 
   public SpringNativeAwsLambdaStack(@NotNull final Construct scope, @NotBlank final String id, @NotNull final StackProps props) {
